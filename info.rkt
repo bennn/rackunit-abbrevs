ad5c002f62afc6d6ed2167cd19306051fdfcb6f6
ben <types@ccs.neu.edu>
2015-10-01 03:20:42 -0400

    ** init

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "rackunit-abbrevs")
+(define deps '("base"))
+(define build-deps '("cover" "cover-coveralls" "scribble-lib" "racket-doc" "rackunit-lib"))
+(define pkg-desc "")
+(define version "0.0")
+(define pkg-authors '(ben))
+(define scribblings '(("scribblings/rackunit-abbrevs.scrbl")))
